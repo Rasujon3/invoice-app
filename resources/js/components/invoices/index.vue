3ccb73648883c3ccc3bc8259b6176afe55c57911
rasujon3@gmail.com <rasujon3@gmail.com>
2024-06-29 02:24:16 +0600

    invoice showing done - 49:00

--- resources/js/components/invoices/index.vue
+++ resources/js/components/invoices/index.vue
@@ -10,7 +10,8 @@ onMounted(async () => {
 const getInvoices = async () => {
     try {
         let response = await axios.get("/api/get_all_invoice");
-        console.log("response", response.data);
+        console.log("response", response.data.invoices);
+        invoices.value = response.data.invoices;
     } catch (error) {
         console.log("error", error);
     }
@@ -76,13 +77,26 @@ const getInvoices = async () => {
                 </div>
 
                 <!-- item 1 -->
-                <div class="table--items">
-                    <a href="#" class="table--items--transactionId">#093654</a>
-                    <p>Jan 18, 9:31am</p>
-                    <p>#093654</p>
-                    <p>Jonathan Yu</p>
-                    <p>Jan 18, 9:31am</p>
-                    <p>$ 16,943</p>
+                <div
+                    class="table--items"
+                    v-for="item in invoices"
+                    :key="item.id"
+                    v-if="invoices.length > 0"
+                >
+                    <a href="#" class="table--items--transactionId"
+                        >#{{ item.id }}</a
+                    >
+                    <p>{{ item.date }}</p>
+                    <p>#{{ item.number }}</p>
+                    <p v-if="item.customer.firstname">
+                        {{ item.customer.firstname }}
+                    </p>
+                    <p v-else></p>
+                    <p>{{ item.due_date }}</p>
+                    <p>$ {{ item.total }}</p>
+                </div>
+                <div class="table--items" v-else>
+                    <p>Invoice not found</p>
                 </div>
             </div>
         </div>
