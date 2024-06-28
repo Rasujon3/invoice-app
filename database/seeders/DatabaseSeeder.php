6f574f70df6eb291ef52f7ac67accc8de812962c
rasujon3@gmail.com <rasujon3@gmail.com>
2024-06-29 01:46:14 +0600

    data showing issue fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,13 +9,22 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-//        \App\Models\Counter::factory(1)->create();
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        //        \App\Models\Counter::factory(1)->create();
 //        \App\Models\Product::factory(5)->create();
 //        \App\Models\Customer::factory(1)->create();
 //        \App\Models\Invoice::factory(4)->create();
-        \App\Models\InvoiceItem::factory(5)->create();
+\App\Models\InvoiceItem::factory(5)->create();
     }
 }
