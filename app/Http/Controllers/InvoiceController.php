c88b5819c4d1e24e06ed5700cae88be63c5d2a4e
rasujon3@gmail.com <rasujon3@gmail.com>
2024-06-30 23:17:43 +0600

    create invoice done

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Counter;
+use App\Models\InvoiceItem;
 use Illuminate\Http\Request;
 use App\Models\Invoice;
 
@@ -60,5 +61,32 @@ class InvoiceController extends Controller
             ]
         ];
         return response()->json($formData);
+    }
+
+    public function add_invoice(Request $request)
+    {
+        $invoiceitem = $request->input("invoice_item");
+
+        $invoicedata['sub_total'] = $request->input("subtotal");
+        $invoicedata['total'] = $request->input("total");
+        $invoicedata['customer_id'] = $request->input("customer_id");
+        $invoicedata['number'] = $request->input("number");
+        $invoicedata['date'] = $request->input("date");
+        $invoicedata['due_date'] = $request->input("due_date");
+        $invoicedata['discount'] = $request->input("discount");
+        $invoicedata['reference'] = $request->input("reference");
+        $invoicedata['terms_and_conditions'] = $request->input("terms_and_conditions");
+
+        $invoice = Invoice::create($invoicedata);
+//        dd('$invoiceitem',$invoiceitem);
+        foreach (json_decode($invoiceitem) as $item) {
+//            dd('$item',$item);
+            $itemdata['product_id'] = $item->id;
+            $itemdata['invoice_id'] = $invoice->id;
+            $itemdata['quantity'] = $item->quantity;
+            $itemdata['unit_price'] = $item->unit_price;
+
+            InvoiceItem::create($itemdata);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvoiceController;
@@ -24,3 +25,5 @@ Route::get('/get_all_invoice', [InvoiceController::class, 'get_all_invoice']);
 Route::get('/search_invoice', [InvoiceController::class, 'search_invoice']);
 Route::get('/create_invoice', [InvoiceController::class, 'create_invoice']);
 Route::get('/customers', [CustomerController::class, 'all_customers']);
+Route::get('/products', [ProductController::class, 'all_products']);
+Route::post('/add_invoice', [InvoiceController::class, 'add_invoice']);
